80439868fffb42930ca2c3d36a1ef6b50431206f
sajid-khan-afridi <69663016+sajid-khan-afridi@users.noreply.github.com>
2020-12-02 16:04:34 +0500

    Update Message.js

--- src/Message.js
+++ src/Message.js
@@ -1,6 +1,6 @@
 import React from 'react';
 export function Message(props){
     return(
-    <h3>Value of counter variable is: {props.counter}</h3>
+    <h3>Value of counter va riable is: {props.counter}</h3>
     );
 }
